56861ef823ecc2c9dad268af5a096d00706a6032
Matthias Moegerle <matthias.moegerle@web.de>
2015-12-11 23:32:37 +0100

    Added Reports+Settings

--- report/SamplingReport.java
+++ report/SamplingReport.java
@@ -1,76 +1,74 @@
 package report;
 
-import core.Coord;
 import core.DTNHost;
+import core.Settings;
+import core.SettingsError;
 import core.SimClock;
-import core.SimScenario;
 import core.UpdateListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Example report that samples the whole simulation on regular intervals.
+ * Abstract class that makes it easier to implement sampling reports.
  *
  * @author teemuk
  */
-public class SamplingReport
+public abstract class SamplingReport
 extends Report
 implements UpdateListener {
 
-
-  //==========================================================================//
-  // Instance vars
-  //==========================================================================//
-  private double lastRecord = Double.MIN_VALUE;
-  private int interval = 100;
-
-  private Coord center;
-  final List <Double> distancesFromOrigin = new ArrayList<>( 10000 );
-  //==========================================================================//
+	//========================================================================//
+	// Settings
+	//========================================================================//
+	/** Interval in seconds between samples ({@value}). */
+	public static final String SAMPLE_INTERVAL_SETTING = "sampleInterval";
+	/** Default value for sample interval ({@value} seconds). */
+	public static final double DEFAULT_SAMPLE_INTERVAL = 60;
+	//========================================================================//
 
 
-  //==========================================================================//
-  // Report
-  //==========================================================================//
-  public SamplingReport() {
-    super();
-    this.init();
-  }
-
-  @Override
-  protected void init() {
-    super.init();
-
-    int worldX = SimScenario.getInstance().getWorldSizeX();
-    int worldY = SimScenario.getInstance().getWorldSizeY();
-
-    this.center = new Coord( worldX / 2, worldY / 2 );
-  }
-
-  @Override
-  public void done() {
-    super.write( "Mean: " + super.getAverage( this.distancesFromOrigin ) );
-    super.write( "Variance: " + super.getVariance( this.distancesFromOrigin ) );
-
-    super.done(); // Closes the report file
-  }
-  //==========================================================================//
+	//========================================================================//
+	// Instance vars
+	//========================================================================//
+	private double lastRecord = Double.MIN_VALUE;
+	protected final double interval;
+	//========================================================================//
 
 
-  //==========================================================================//
-  // UpdateListener
-  //==========================================================================//
-  @Override
-  public void updated( final List <DTNHost> hosts ) {
-    if ( SimClock.getTime() - lastRecord < interval ) return;
-    lastRecord = SimClock.getTime();
+	//========================================================================//
+	// Subclass API
+	//========================================================================//
+	protected abstract void sample(final List<DTNHost> hosts);
+	//========================================================================//
 
-    double totalDistances = 0;
-    for ( final DTNHost host : hosts ) {
-      totalDistances += this.center.distance( host.getLocation() );
-    }
-    this.distancesFromOrigin.add( totalDistances / hosts.size() );
-  }
-  //==========================================================================//
+
+	//========================================================================//
+	// Report
+	//========================================================================//
+	protected SamplingReport() {
+		super();
+
+		// Set the sampling interval
+		final Settings settings = super.getSettings();
+		this.interval = settings.getDouble(SAMPLE_INTERVAL_SETTING,
+				DEFAULT_SAMPLE_INTERVAL);
+		if (this.interval <= 0) {
+			throw new SettingsError("Setting '" + SAMPLE_INTERVAL_SETTING
+				+ "' must be positive. Found " + this.interval + ".");
+		}
+	}
+	//========================================================================//
+
+
+	//========================================================================//
+	// UpdateListener
+	//========================================================================//
+	@Override
+	public void updated(final List<DTNHost> hosts) {
+		if (SimClock.getTime() - lastRecord < interval) return;
+		lastRecord = SimClock.getTime();
+
+		this.sample(hosts);
+	}
+	//========================================================================//
 }
